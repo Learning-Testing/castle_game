7ca02d6a2822e5ca0f1837a1911a7ffa91dbbefc
Sean M <scmwhr83@gmail.com>
2020-01-07 15:21:35 -0500

    Create castle_game.cpp

---
+++ castle_game.cpp
@@ -0,0 +1,138 @@
+// castlegame.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+class Unit_Values
+{
+private:
+	int x_val, y_val, z_val;
+
+
+public:
+	int health, armor, move_speed, experience, attack_range;
+	std::string damage_type, name;  // ranged/melee/(un/)mounted
+
+	void set_unit_location(int x, int y, int z)
+	{
+		x_val = x;
+		y_val = y;
+		z_val = z;
+	}
+
+	int getX(void)
+	{
+		return x_val;
+	}
+
+	int getY(void)
+	{
+		return y_val;
+	}
+
+	int getZ(void)
+	{
+		return z_val;
+	}
+};
+
+class Soldier : public Unit_Values
+{
+public:
+	Soldier() 
+	{
+		health = 100;
+		armor = 50;
+		move_speed = 25;
+		experience = 10;
+		attack_range = 1;
+		name = "Soldier";
+		damage_type = "Melee";
+	}
+};
+
+class Archer : public Unit_Values
+{
+public:
+	Archer()
+	{
+		health = 25;
+		armor = 5;
+		move_speed = 35;
+		experience = 25;
+		attack_range = 5;
+		name = "Archer";
+		damage_type = "Ranged";
+	}
+};
+
+
+class warmap
+{
+public:
+	int map_size = 200;
+	
+	void map_movement(Unit_Values)
+	{
+		;
+	}
+};
+
+
+void set_unit_positions(std::vector<Unit_Values> units, int map_size)
+{
+	int x_val = 0, y_val = 0;
+	for (Unit_Values unit : units)
+	{
+		
+		unit.set_unit_location(x_val, y_val, 0);
+
+		std::cout << unit.getX() << " " << unit.getY() << " " << unit.getZ() << std::endl;
+		
+		if ((x_val > map_size) && (x_val % map_size == 0 || 1))
+		{
+			//std::cout << "\ny change from " << y_val << " to ";
+			x_val = 0;
+			++y_val;
+			//std::cout << y_val << "\n";
+		}
+		//std::cout << "x value:" << x_val << ", ";
+		x_val++;
+	}
+}
+
+std::vector<Unit_Values> redeem_points(int num)
+{
+	std::vector<Unit_Values> unit_vector;
+	Soldier curr_unit;
+	 for (int i = 0; i < num; ++i)
+	{
+		Soldier curr_unit;
+		unit_vector.push_back(curr_unit);
+		//std::cout << curr_unit.health << " " << curr_unit.health << " " << curr_unit.damage_type << "\n";
+	} 
+	return unit_vector;
+}
+
+
+int main()
+{
+	warmap our_map;
+
+	std::vector<Unit_Values> our_units = redeem_points(1600);
+
+	set_unit_positions(our_units, our_map.map_size);
+	
+	for (Unit_Values unit : our_units)
+	{
+		std::cout << unit.health << "\n";
+		std::cout << unit.getX() << " " << unit.getY() << " " << unit.getZ() << std::endl;
+		//std::cout << "(" << unit.getX() << ", " << unit.getY() << ", " << unit.getZ() << ")" << std::endl;
+	}
+	
+	return 0;
+}
